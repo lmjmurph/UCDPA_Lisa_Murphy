738ed22b4932229820818756f8eb8ecbee1829d2
Lisa.Murphy <lisamurphy@gmail.com>
2021-10-23 02:49:30 +0100

    Add further EDA

--- Heart_Disease.py
+++ Heart_Disease.py
@@ -46,12 +46,18 @@ print('\nIDENTIFY MISSING VALUES: \n', heart_disease.isnull().sum(), '')
 heart_disease.drop_duplicates(inplace=True)
 print('\nDATA SHAPE AFTER REMOVING DUPLICATES: \n', heart_disease.shape, '')
 
+
+sns.boxplot(x="Sex", hue="HeartDisease", y="Cholesterol", data=heart_disease)
+# adding a title to the plot:
+plt.title('Renme', fontsize=14)
+# displaying the plot:
+plt.show()
+
 # Look closer at Cholesterol values - min value = 0 doesn't make sense
 print(heart_disease['Cholesterol'].value_counts().sort_index())
 heart_disease['Cholesterol'].replace([0],np.nan, inplace=True)
 
 describe_all(heart_disease)
-
 
 sns.boxplot(x="Sex", hue="HeartDisease", y="Cholesterol", data=heart_disease)
 # adding a title to the plot:
@@ -60,5 +66,42 @@ plt.title('Renme', fontsize=14)
 plt.show()
 
 
+# to facilitate charts for categorical data vs numerical data divide the dataframe
+cat_data = heart_disease.select_dtypes(include='object')
+num_data = heart_disease.select_dtypes(exclude='object')
+print("\nCategorical fields: \n", cat_data.columns, "\n\nNumerical fields: \n", num_data.columns)
+
+# create function to do countplot of different features vs Heart Disease
+def cplot(col):
+    plt.figure()
+    sns.countplot(x=col, hue='HeartDisease', data=heart_disease, palette='RdBu')
+    plt.xticks(list(range(heart_disease[col].unique().size)), list(heart_disease[col].unique()))
+    plt.show()
+
+for i in cat_data:
+    cplot(i)
+num_data_cols = num_data.columns.values
+num_data_cols = np.append(num_data_cols,'HeartDisease')
+print(num_data_cols)
+sns.pairplot(heart_disease.loc[:,num_data_cols], hue='HeartDisease')
+plt.show()
+
+# To use scikit learn we need to encode categorical features - use get_dummies()
+heart_disease_encoded = pd.get_dummies(heart_disease, drop_first=True)
+print(heart_disease_encoded.head())
+print(heart_disease_encoded.info())
+
+
+# Create feature and target arrays
+X = heart_disease_encoded.drop('HeartDisease_1', axis=1)
+y = heart_disease_encoded['HeartDisease_1']
+print(X)
+print(y)
+#y = heart_disease_encoded['HeartDisease_1'].values()
+#print(y)
+#_ = pd.plotting.scatter_matrix(X, c = y, marker = 'D')
+
+
+#
 
 
